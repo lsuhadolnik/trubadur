5040cdd36b8c33a4af43bcc02e42a24019f2544a
volkmaster <ziga.vucko@gmail.com>
2018-01-04 01:04:35 +0100

    https fix 3

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
 
-        if (App::environment('production')) {
+        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
