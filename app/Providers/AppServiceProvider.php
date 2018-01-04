3dfe4f3fba4d49f009b03d0a4ae63afe346581bc
volkmaster <ziga.vucko@gmail.com>
2018-01-04 01:01:51 +0100

    https fix 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,12 +13,12 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+    public function boot()
     {
         Schema::defaultStringLength(191);
 
-        if (env('APP_ENV') !== 'local') {
-            $url->forceSchema('https');
+        if (App::environment('production')) {
+            URL::forceScheme('https');
         }
     }
 
