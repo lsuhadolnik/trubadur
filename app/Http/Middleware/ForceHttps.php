6ddd56dedd137c468043d8a75088d20abc7f709d
volkmaster <ziga.vucko@gmail.com>
2018-03-30 19:48:46 +0200

    https redirection fix

--- app/Http/Middleware/ForceHttps.php
+++ app/Http/Middleware/ForceHttps.php
@@ -16,7 +16,7 @@ class ForceHttps
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && env('APP_ENV') === 'production') {
-            return redirect()->secure($request->getRequestUri(), 301);
+            return redirect()->secure($request->getRequestUri());
         }
 
         return $next($request);
